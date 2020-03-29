0ff8908c4f4458136b6f3eea6419c6c627d2a8ec
Adzkar Fauzie <adzkarfauzie02@gmail.com>
2020-03-30 02:18:05 +0700

    Fixed Emoji Issue

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import { FooterWrapper } from "./style";
 
 const Index = () => {
-  return <FooterWrapper>Made with ❤️ in Solo, Indonesia</FooterWrapper>;
+  return (
+    <FooterWrapper>
+      Made with{" "}
+      <span role="img" aria-label="sayang kamu heueheu">
+        ❤️
+      </span>{" "}
+      in Solo, Indonesia
+    </FooterWrapper>
+  );
 };
 
 export default Index;
